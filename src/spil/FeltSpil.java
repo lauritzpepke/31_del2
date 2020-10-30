f66afc3ce34259f13a333bbcdf4fd38587907a09
Nadia <s201369@student.dtu.dk>
2020-10-30 19:01:54 +0100

    Jeg arbejder med UC1. Generer 2 spillere, der skal have unikke navne. Ikke færdig, holder mad pause. Jeg har klart markeret med /* hvor jeg er i de to klasser, jeg arbejder i
    Har gjort UI'en static.

---
+++ src/spil/FeltSpil.java
@@ -0,0 +1,41 @@
+package spil;
+
+/**
+ * Repræsenterer selve spillet
+ */
+public class FeltSpil
+{
+    public static void main(String[] args)
+    {
+        String navnSpillerEt;
+        String navnSpillerTo;
+
+
+        // UC1 Spillet kan kunne initiere et nyt spil terningspil
+        // mellem to personer
+
+
+        // Spillerne skal indskrive deres unikke navne
+        navnSpillerEt = UI.navngiv(1);
+        navnSpillerTo = UI.navngiv(2);
+
+        /* madpause HER
+        // check om navnene er ens:
+        if (navnSpillerEt == navnSpillerTo)
+        {
+            UI.sasammeNavnFejlbesked
+        }
+        */
+
+
+        // Når spillet starter skal to spillere genereres.
+        //public Spiller spillerEt = new Spiller();
+
+        // Spillerne skal indskrive deres navne, som skal være forskellige fra hinanden
+
+        //Spillerne skal have deres start pengebeholdning af 1000 på deres konto.
+
+    }
+
+
+}
--- src/spil/UI.java
+++ src/spil/UI.java
@@ -3,7 +3,32 @@ Al user interface skal samles her, så det er let at oversætte sprog fremover.
  */
 package spil;
 
+import java.util.Scanner;
+
 public class UI
 {
+    //private String navnSpillerEt;
+    //private String navnSpillerTo;
+
+    static Scanner input = new Scanner(System.in);
+
+    /** navngiver en spiller efter brugerinput
+     * @return En strengværdi med det valgte navn
+     */
+    static String navngiv(int spillerNummer)
+    {
+        System.out.println("Spiller " + spillerNummer + " indtast dit navn: ");
+        return input.next();
+    }
+
+    /* madpause HER
+    static String sammeNavnFejlbesked(String navn1, String navn2)
+
+    {
+        System.out.println();
+    }
+    */
+
+
 
 }
