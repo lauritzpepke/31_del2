5ec9dcd6806ff8e805986fd49deefc0d88ad8509
Nadia <s201369@student.dtu.dk>
2020-10-30 15:39:29 +0100

    Jeg har oprettet klassen Felt og har skrevet JavaDoc kommentering for dokumentation. Jeg har ryddet lidt op i min start kommentar.

--- src/spil/Felt.java
+++ src/spil/Felt.java
@@ -1,26 +1,75 @@
 package spil;
 /*
-(Man kan ikke slå 1 med to terninger)
-Tower 		+250
-Crater 		-100
-Palace gates		+100
-Cold Desert	 	-20
-Walled city 		+180
-Monastery 		0
-Black cave 		-70
-Huts in the mountain 		+60
-The Werewall (werewolf-wall)		-80,	men spilleren får en ekstra tur.
-The pit 		-50
-Goldmine 		+650
-
+Eksempel på et felt:
+2   Tower   +250
  */
 
 /**
- * Repræsenter et felt i spillet
+ * Repræsenter et felt i spillet.
  */
 public class Felt
 {
-    public String navn;
+    private String navn;
+    private String effekt;
+    private int nummer;
 
+    /** Lavet er felt med det specificerede navn, effekt og nummer.
+     * @param navn Feltets navn.
+     * @param effekt Feltets effekt.
+     * @param nummer Feltets nummer på brættet.
+     */
+    public Felt(String navn, String effekt, int nummer)
+    {
+        this.navn = navn;
+        this.effekt = effekt;
+        this.nummer = nummer;
+    }
 
+    /** Henter feltets navn.
+     * @return En string, der repræsenterer feltets navn.
+     */
+    public String getNavn()
+    {
+        return navn;
+    }
+
+    /** Sætter feltets navn
+     * @param navn En string, der indeholder feltets navn.
+     */
+    public void setNavn(String navn)
+    {
+        this.navn = navn;
+    }
+
+    /** Henter feltets effekt
+     * @return En string, der repræsenterer feltets effekt.
+     */
+    public String getEffekt()
+    {
+        return effekt;
+    }
+
+    /** Sætter feltets effekt.
+     * @param effekt En string, der indeholder feltets effekt.
+     */
+    public void setEffekt(String effekt)
+    {
+        this.effekt = effekt;
+    }
+
+    /** Henter feltets nummer.
+     * @return En string, der repræsenterer feltets nummer på brættet.
+     */
+    public int getNummer()
+    {
+        return nummer;
+    }
+
+    /** Sætter feltets nummer.
+     * @param nummer En int, der indeholder feltets nummer på brættet.
+     */
+    public void setNummer(int nummer)
+    {
+        this.nummer = nummer;
+    }
 }
