6ff721d551038a54e55c7a51d95832bbbac823fc
Nadia <s201369@student.dtu.dk>
2020-10-30 21:09:55 +0100

    Nu generer spillet to spillere ved start og tildeler dem navne.

--- src/spil/FeltSpil.java
+++ src/spil/FeltSpil.java
@@ -5,35 +5,51 @@ package spil;
  */
 public class FeltSpil
 {
+    UI spilUI = new UI();
+
+    String navnSpillerEt;
+    String navnSpillerTo;
+
     public String feltSpil()
     {
-        UI spilUI = new UI();
-
-        String navnSpillerEt;
-        String navnSpillerTo;
-
-
-        // UC1 Spillet kan kunne initiere et nyt spil terningspil
-        // mellem to personer
-
-
-        // Spillerne skal indskrive deres unikke navne
-        navnSpillerEt = spilUI.navngiv(1);
-        navnSpillerTo = spilUI.navngiv(2);
-
-        // check om navnene er ens indtil de ikke er
-        while (navnSpillerEt.equals(navnSpillerTo))
-        {
-            navnSpillerTo = spilUI.sammeNavnFejlbesked();
-        }
-
         // Når spillet starter skal to spillere genereres.
-        //public Spiller spillerEt = new Spiller(navnSpillerEt);
-
-        // Spillerne skal indskrive deres navne, som skal være forskellige fra hinanden
+        Spiller spiller1 = new Spiller(genererSpiller(1));
+        Spiller spiller2 = new Spiller(genererSpiller(2));
 
         //Spillerne skal have deres start pengebeholdning af 1000 på deres konto.
 
+
+        return kørSpil(spiller1, spiller2);
+    }
+
+    // UC1 Spillet kan kunne initiere et nyt spil terningspil
+    // mellem to personer
+     public String genererSpiller(int spillerNummer)
+     {
+         if (spillerNummer == 1)
+         {
+             // Spillerne skal indskrive deres unikke navne
+             return spilUI.navngiv(spillerNummer);
+         }
+         else if (spillerNummer == 2)
+         {
+             navnSpillerTo = spilUI.navngiv(2);
+
+             // check om navnene er ens indtil de ikke er
+             while (navnSpillerEt.equals(navnSpillerTo))
+             {
+                 navnSpillerTo = spilUI.sammeNavnFejlbesked();
+             }
+             return navnSpillerTo;
+         }
+         //den skal ikke nå hertil.
+         return "N/A spillernummer fejl";
+     }
+
+     // spil til vinder findes og returner denne
+    public String kørSpil(Spiller et, Spiller to)
+    {
+        // ...
 
         return "Vinder fundet SKAL ÆNDRES";
     }
