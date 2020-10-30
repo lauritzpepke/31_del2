4f0b35a9dba7090f1714dc19c1ff414b4c462956
Nadia <s201369@student.dtu.dk>
2020-10-30 17:47:41 +0100

    Tilføjet constructor, getter, setter og JavaDoc dokumentation for spiller.

--- src/spil/Spiller.java
+++ src/spil/Spiller.java
@@ -4,4 +4,27 @@ public class Spiller
 {
     public String navn;
 
+    /** Laver en spiller med det specificerede navn.
+     * @param navn Spillerens navn
+     */
+    public Spiller(String navn)
+    {
+        this.navn = navn;
+    }
+
+    /** Henter spillerens navn.
+     * @return En string, der repræsenterer spillerens navn.
+     */
+    public String getNavn()
+    {
+        return navn;
+    }
+
+    /** Sætter spillerens navn
+     * @param navn En string, der indeholder spillerens navn.
+     */
+    public void setNavn(String navn)
+    {
+        this.navn = navn;
+    }
 }
