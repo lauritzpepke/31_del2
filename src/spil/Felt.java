e7a9977ccd2086fbf8c173433b0592e7f5f1211a
Nadia <s201369@student.dtu.dk>
2020-10-30 15:26:04 +0100

    Jeg har oprettet klassen Felt og er begyndt at bruge JavaDoc kommentering for dokumentation

---
+++ src/spil/Felt.java
@@ -0,0 +1,26 @@
+package spil;
+/*
+(Man kan ikke slå 1 med to terninger)
+Tower 		+250
+Crater 		-100
+Palace gates		+100
+Cold Desert	 	-20
+Walled city 		+180
+Monastery 		0
+Black cave 		-70
+Huts in the mountain 		+60
+The Werewall (werewolf-wall)		-80,	men spilleren får en ekstra tur.
+The pit 		-50
+Goldmine 		+650
+
+ */
+
+/**
+ * Repræsenter et felt i spillet
+ */
+public class Felt
+{
+    public String navn;
+
+
+}
