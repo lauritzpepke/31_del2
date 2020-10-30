1dfa1250288095f26e9192448fbbb22a903e2dcd
Nadia <s201369@student.dtu.dk>
2020-10-30 17:30:33 +0100

    Tilføjet JavaDoc til Konto

--- src/spil/Konto.java
+++ src/spil/Konto.java
@@ -1,18 +1,31 @@
 package spil;
+
+/**
+ * Repræsenterer konto for spillernes pengebeholdning i spillet.
+ */
 public class Konto
 {
-    public int pengebeholdning;
+    private int pengebeholdning;
 
+    /** Laver en konto med pengebeholdning
+     * @param pengebeholdning Kontoens pengebeholdning
+     */
     public Konto(int pengebeholdning)
     {
         this.pengebeholdning = pengebeholdning;
     }
 
+    /** Henter kontoens pengebeholdning
+     * @return en int, der repræsenterer kontoens pengebeholdning
+     */
     public int getPengebeholdning()
     {
         return pengebeholdning;
     }
 
+    /** Sætter kontoens pengebeholdning.
+     * @param pengebeholdning en int, der indeholder kontoens pengebeholdning
+     */
     public void setPengebeholdning(int pengebeholdning)
     {
         this.pengebeholdning = pengebeholdning;
